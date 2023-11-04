af83566a0d15772df4701d22fae78b72af7ed4e7
Diogo <diogoazvd77@gmail.com>
2023-11-04 12:10:03 -0400

    new file:   TODO.txt 	deleted:    app/favicon.ico 	deleted:    app/globals.css 	deleted:    app/layout.js 	deleted:    app/page.js 	deleted:    app/page.module.css 	new file:   files/css/index.css 	new file:   files/images/background.png 	new file:   files/images/backgroundBlack.png 	new file:   files/images/background_4k.png 	new file:   files/images/drinks.png 	new file:   files/images/mousse.png 	new file:   files/images/noise.png 	modified:   package-lock.json 	modified:   package.json 	new file:   pages/_app.js 	new file:   pages/index.jsx 	deleted:    public/next.svg 	new file:   public/slide.js 	deleted:    public/vercel.svg

---
+++ pages/index.jsx
@@ -0,0 +1,447 @@
+import { useEffect } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+let total = 0;
+let itemId = 0;
+
+export default function Main() {
+    return (
+        <div id="main">
+            <Navbar />
+            <Logo />
+            <Sales />
+            <Menu />
+            <HoursAndLocations />
+            <Cart />
+            <Reservations />
+        </div>
+    );
+}
+
+function Navbar() {
+    function showElement(id) {
+        const element = document.getElementById(id);
+        element.style.display = "flex";
+    }
+
+    function navbar() {
+        const navbar = document.getElementById("navbar");
+        const navbarButtonShow = document.getElementById("navbarButtonShow");
+
+        if (navbar.style.display == "none") {
+            navbar.style.display = "flex";
+            navbarButtonShow.style.display = "none";
+        }
+        
+        else {
+            navbar.style.display = "none";
+            navbarButtonShow.style.display = "flex";
+        }
+    }
+
+    function scrollTo(section, offset) {
+        const viewHeight = window.innerHeight * section + (window.innerHeight * section * offset);
+        window.scroll({
+            top: viewHeight,
+            left: 0,
+            behavior: "smooth"
+        });
+    }
+
+    return (
+        <>
+            <div id="navbar">
+                <span onClick={() => {scrollTo(0, 0)}}>Home</span>
+                <span onClick={() => {scrollTo(1, .15)}}>Sales</span>
+                <span onClick={() => {scrollTo(2, .12)}}>Menu</span>
+                <span onClick={() => {showElement("hoursAndLocationsBox")}}>Hours & Locations</span>    
+                <span onClick={() => {showElement("cartBox")}}>Cart</span>
+                <span id="navbarButtonHide" onClick={navbar}>Hide</span>
+                <div id="reservationButton" onClick={() => {scrollTo(3, .11)}}>Reservations</div>
+            </div>
+
+            <div id="navbarButtonShow" onClick={navbar}>Show</div>
+        </>
+    );
+}
+
+function Logo() {
+    function scrollTo(section) {
+        const viewHeight = window.innerHeight * section + (window.innerHeight * section * .15);
+        window.scroll({
+            top: viewHeight,
+            left: 0,
+            behavior: "smooth"
+        });
+    }
+
+    return (
+        <div id="logoBox">
+            <div id="logo">Naga</div>
+            <div id="cta" onClick={() => scrollTo(1)}>Check the sales</div>
+        </div>
+    );
+}
+
+function Sales() {
+    useEffect(() => {
+        const script = document.createElement("script");
+
+        script.src = "slide.js";
+        script.async = true;
+
+        document.body.appendChild(script);
+    }, []);
+
+    useEffect(() => {
+        const salesNavLeft = document.getElementById("salesNavLeft");
+        const salesNavRight = document.getElementById("salesNavRight");
+
+        salesNavLeft.addEventListener("click", () => {
+            onClickAnimate("left");
+        });
+
+        salesNavRight.addEventListener("click", () => {
+            onClickAnimate("right");
+        });
+    })
+
+    return (
+        <Background content = {
+            <div id="sales">
+                <div id="salesTitle">Sales!</div>
+
+                <div id="salesContent">
+                    <div id="salesDescription">
+                        <div id="salesDescriptionTitle0" style={{opacity: 1}}>Cocktail 30% off!</div>
+                        <div id="salesDescriptionText0" style={{opacity: 1}}>You can order custom cocktails! Check the menu for more information!</div>
+                        <div id="salesDescriptionTitle1" style={{opacity: 0}}>2 Mousse for 75% the price!</div>
+                        <div id="salesDescriptionText1" style={{opacity: 0}}>Offer valid til 18/11/2023</div>
+                    </div>
+
+                    <div id="salesImage">
+                        <div id="item0" style={{opacity: 1, zIndex: 1}}></div>
+                        <div id="item1" style={{opacity: 0, zIndex: 0}}></div>
+                    </div>
+                </div>
+
+                <div id="salesNav">
+                    <div id="salesNavLeft">&lt;</div>
+
+                    <div id="salesNavItems">
+                        <div id="current_item0"></div>
+                        <div id="current_item1"></div>
+                    </div>
+                    
+                    <div id="salesNavRight">&gt;</div>
+                </div>
+            </div>
+        }/>
+    );    
+}
+
+function Menu() {
+    return (
+        <Background content = {
+            <div id="menuBox">
+                <div id="menuTitle">
+                    <div>Menu</div>
+
+                    <div id="menuInfo">?</div>
+
+                    <div id="menuInfoDescription">
+                        <ul>
+                            <li>Hover over an item to see its details</li>
+                            <li>Add an item to the cart by clicking on its price</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div id="menu">
+                    <MenuGrid title = "Main Course" content = {
+                        <>
+                            <MenuGridItem id="beef" name = "Roast Beef with Vegetables" price = "12.99" details = "How would you like your steak?"/>
+                        </>
+                        
+                    }/>
+
+                    <MenuGrid title = "Drinks" content = {
+                        <>
+                            <MenuGridItem id="cocktail"  name = "Custom Cocktail" price = "4.99" details = "Flavors: ..."/>
+                            <MenuGridItem id="beer"  name = "Beer" price = "1.99" details = "Drink with moderation!"/>
+                        </>
+                    }/>
+
+                    <MenuGrid title = "Desserts" content = {
+                        <>
+                            <MenuGridItem id="chocolateMousse" name = "Chocolate Mousse" price = "2.99" details = "Placeholder"/>
+                        </>
+                    }/>
+                </div>
+            </div>
+        }/>
+    );
+}
+
+function Background(data) {
+
+    useEffect(() => {
+        const menuInfo = document.getElementById("menuInfo");
+        const description = document.getElementById("menuInfoDescription");
+
+        menuInfo.addEventListener("mouseover", () => {            
+            description.style.display = "flex";
+        })
+
+        menuInfo.addEventListener("mouseout", () => {            
+            description.style.display = "none";
+        })
+    }, [])    
+
+    return (
+        <div id="background">
+            <div id="backgroundFilter">
+                {data["content"]}
+            </div>
+        </div>
+    );
+}
+
+function MenuGrid(data) {
+    return (
+        <div id="menuGrid">
+            <div>{data["title"]}</div>
+            {data["content"]}
+        </div>
+    );
+}
+
+function MenuGridItem(data) {
+    useEffect(() => {
+        const element = document.getElementById(data["id"]);
+        const showElement = document.getElementById(data["id"] + "_details");            
+        
+        element.addEventListener("mouseover", () => {
+            showElement.style.display = "flex";
+        })
+
+        element.addEventListener("mouseout", () => {
+            showElement.style.display = "none";
+        })
+
+    },[]);
+
+    function addItem(name, price) {
+        const cartItem = document.getElementById("cartItem");
+        const cartTitle = document.getElementById("cartTitle");
+
+        const newDiv = document.createElement("div");
+        newDiv.id = "menu" + itemId;
+
+        const info = document.createElement("div");
+        const infoText = document.createTextNode(name + " " + "$" + price);
+        info.appendChild(infoText);
+
+        const button = document.createElement("div")
+        const buttonText = document.createTextNode("-");
+        let tmpNumber = itemId
+        button.id = "minus" + itemId;
+        button.className = "minus";
+        button.appendChild(buttonText);
+
+        newDiv.appendChild(info);
+        newDiv.appendChild(button);
+
+        cartItem.appendChild(newDiv);
+
+        total = total + Number(price);
+        const totalString = total.toLocaleString("en-US", { maximumFractionDigits: 2, minimumFractionDigits: 2 });
+        cartTitle.textContent = `Cart - Total: $${totalString}`;
+
+        const minusButton = document.getElementById("minus" + tmpNumber);
+        minusButton.addEventListener("click", () => {
+            const removeElement = document.getElementById("menu" + tmpNumber);
+            removeElement.remove();     
+            total = total - Number(price);
+            const totalString = total.toLocaleString("en-US", { maximumFractionDigits: 2, minimumFractionDigits: 2 });
+            cartTitle.textContent = `Cart - Total: $${totalString}`;      
+        });
+
+        itemId++;
+    }
+
+    return (
+        <div id="menuGridItem">
+            <div id={data["id"]} className="menuItem">{data["name"]}</div>
+            <div className="menuPrice" onClick={() => addItem(data["name"], data["price"])}>${data["price"]}</div>
+            <div className="details" id={data["id"] + "_details"}>{data["details"]}</div>
+        </div>
+    );
+}
+
+function HoursAndLocations() {
+    function hide() {
+        const element = document.getElementById("hoursAndLocationsBox");
+        element.style.display = "none";
+    }
+
+    return (
+        <div id="hoursAndLocationsBox">
+            <div id="hoursAndLocations">
+                <div id="hoursAndLocationsTitle">Hours And Locations</div>
+
+                <div id="hoursAndLocationsGrid">
+                    <div>Monday - Friday | 18:00 - 00:00 PM | Any Place</div>
+                    <div>Saturday - Sunday | 18:00 - 22:00 PM | Any Place</div>
+                </div>
+
+                <div id="border"></div>
+                <div id="border1"></div>
+                <div id="border2"></div>
+            </div>
+
+            <div id="hoursAndLocationsButton" onClick={hide}>X</div>
+            <div id="hoursAndLocationsBackground"></div>
+        </div>
+    );
+}
+
+function Cart() {
+    function hide() {
+        const element = document.getElementById("cartBox");
+        element.style.display = "none";
+    }
+
+    return (
+        <div id="cartBox">
+            <div id="cart">
+                <div id="cartItemBox">
+                    <div id="cartTitle">
+                        <div>Cart - Total: $0</div>
+                    </div>
+
+                    <div id="cartItem">
+                    </div>
+                </div>
+                
+
+                <form id="cartForm">
+                    <div id="cartInfo">
+                        <input type="text" placeholder="Street - Unit Number - Zip Code"></input>   
+                    </div>
+
+                    <div id="cartInfo">
+                        <input type="text" placeholder="Code"></input>
+                    </div>
+
+                    <div id="cartInfo">
+                        <input placeholder="Additional Details"></input>
+                    </div>
+
+                    <div id="cartButton">
+                        <input type="submit" value={"Submit"}></input>
+                    </div>
+
+                    <div id="cartClose" onClick={hide}>X</div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+function Reservations() {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednessday" , "Thursday", "Friday", "Saturday" ]
+    const month = ["-", "-" , "-"];
+
+    for (let i = 1; i < 31; i++) {
+        month.push(i);
+    }
+
+    function select(element) {
+        const allDays = document.querySelectorAll(".allDays");
+        const day = element.target;
+
+        allDays.forEach((e) => {
+            e.style.backgroundColor = "";
+        });
+
+        day.style.backgroundColor = "#090";
+    }
+
+    useEffect(() => {
+        const reservationHourButton = document.getElementById("reservationHourButton");
+        const reservationHourOption = document.getElementById("reservationHourOption");
+        const reservationButtonDetail = document.getElementById("reservationHourButtonDetail");
+
+        reservationHourButton.addEventListener("click", () => {
+            if (reservationHourOption.style.display == "grid") {
+                reservationHourOption.style.display = "none"
+                reservationButtonDetail.style.transform = "rotate(0deg)";
+            }
+
+            else {
+                reservationHourOption.style.display = "grid";
+                reservationButtonDetail.style.transform = "rotate(180deg)";
+            }
+        });
+
+    }, []);
+
+    function selectHour(element) {
+        const reservationHourButton = document.getElementById("reservationHourButton");
+        const hour = element.target.textContent;
+        const reservationHourOption = document.getElementById("reservationHourOption");
+        const reservationButtonDetail = document.getElementById("reservationHourButtonDetail");
+
+        reservationHourButton.textContent = hour;
+
+        if (reservationHourOption.style.display == "grid") {
+            reservationHourOption.style.display = "none"
+            reservationButtonDetail.style.transform = "rotate(0deg)";
+        }
+
+        else {
+            reservationHourOption.style.display = "grid";
+            reservationButtonDetail.style.transform = "rotate(180deg)";
+        }
+    }
+
+    return (
+            <Background content = {
+                <div id="reservation">
+                    <div id="reservationCalendarBox">
+                        <div id="reservationCalendarTitle">November</div>
+
+                        <div id="reservationCalendar">
+                            {days.map((day) => <div key={uuidv4()}>{day}</div>)}
+                            {month.map((day) => <div key={uuidv4()} onClick={(e) => select(e)} className="allDays">{day}</div>)}
+                        </div>
+                        
+                        <div id="reservationHour">
+                            <div id="reservationHourButton">Choose a Hour</div>
+                            <div id="reservationHourButtonDetail"></div>
+
+                            <div id="reservationHourOption">
+                                <span onClick={(e) => selectHour(e)}>6:30PM</span>
+                                <span onClick={(e) => selectHour(e)}>7:30PM</span>
+                                <span onClick={(e) => selectHour(e)}>8:00PM</span>
+                                <span onClick={(e) => selectHour(e)}>8:30PM</span>
+                                <span onClick={(e) => selectHour(e)}>9:00PM</span>
+                                <span onClick={(e) => selectHour(e)}>9:30PM</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <form id="reservationForm">
+                        <input type="text" placeholder="Full Name"></input>
+                        <input type="text" placeholder="Phone Number"></input>
+                        <input type="text" placeholder="E-mail"></input>
+                        <textarea placeholder="Occasion, Requests..."></textarea>
+                        <input type="submit" value={"Submit"}></input>
+                    </form>  
+
+                    <div id="reservationTitle">Reservation</div>            
+                </div>
+            }/>       
+    );
+}
---
+++ public/slide.js
@@ -0,0 +1,93 @@
+let currentItem = 0;
+let numberOfItems = 2;
+let animationTimer;
+
+const leftPattern = [];
+const zIndexList = [];
+
+for (let i = 0; i < numberOfItems; i++) {
+    leftPattern.push(i == 0 ? numberOfItems - 1 : i - 1);
+    zIndexList.push(i == 0 ? i : numberOfItems - i)
+}
+
+function setItemIndex(direction) { 
+    if (direction == "right") {
+        for (let i = 0; i < numberOfItems; i++) {
+            let item = itemId(i);
+            item.style.zIndex = zIndexList[i];
+        }
+      
+        currentItem = (currentItem + 1) % numberOfItems;
+    }
+    
+    else {
+        for (let i = 0, j = numberOfItems; j > 0; i++, j--) {
+            let item = itemId(i);
+            item.style.zIndex = zIndexList[j % numberOfItems];
+        } 
+      
+        currentItem = leftPattern[currentItem];
+    }
+    
+    animationTimer = setTimeout(() => {
+        animate("right");
+    }, 7000);
+}
+
+function animateItem(direction) {
+    let item = itemId(0);
+    let nextItem;
+
+    let circle = document.getElementById("current_item" + currentItem);
+    let nextCircle;
+    
+    let title = document.getElementById("salesDescriptionTitle" + currentItem); 
+    let nextTitle;
+
+    let text = document.getElementById("salesDescriptionText" + currentItem);
+    let nextText;
+
+    if (direction == "right") {    
+        nextItem = itemId(1);
+        nextCircle = document.getElementById("current_item" + ((currentItem + 1) % numberOfItems));
+        nextTitle = document.getElementById("salesDescriptionTitle" + ((currentItem + 1) % numberOfItems));
+        nextText = document.getElementById("salesDescriptionText" + ((currentItem + 1) % numberOfItems));
+    }
+
+    else {
+        nextItem = itemId(leftPattern[currentItem] - currentItem);
+        nextCircle = document.getElementById("current_item" + leftPattern[currentItem]);
+        nextTitle = document.getElementById("salesDescriptionTitle" + leftPattern[currentItem]);
+        nextText = document.getElementById("salesDescriptionText" + leftPattern[currentItem]);
+    }
+  
+    item.style.opacity = 0;
+    nextItem.style.opacity = 1;
+
+    circle.style.backgroundColor = "transparent";
+    nextCircle.style.backgroundColor = "#fff";  
+    
+    title.style.opacity = "0";
+    nextTitle.style.opacity = "1";
+
+    text.style.opacity = "0";
+    nextText.style.opacity = "1";
+}
+
+function itemId(number) {
+    let item = document.getElementById("item" + (currentItem + number) % numberOfItems);
+    return item;
+}
+
+function onClickAnimate(direction) {
+    clearTimeout(animationTimer);
+    clearTimeout(animationStart);
+    animate(direction);   
+}
+
+function animate(direction) {
+    animateItem(direction);
+    setItemIndex(direction);
+}
+
+let animationStart = setTimeout(() => animate("right"), 7000);
